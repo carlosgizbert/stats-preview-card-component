0ed77680fb0531a74d65fab94700f3f4a87ad76f
carlos gizbert <carlosgizbert@gmail.com>
2021-11-17 18:38:46 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card">
+        <span className="card-img"></span>
+        <div className="card-body">
+          <h1>
+            Get <span className="purple">insights</span> that help your business
+            grow.
+          </h1>
+          <p className="card-text grey">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div class="mini-info">
+            <div className="mini-information">
+              <h1>10k+</h1>
+              <p>COMPANIES</p>
+            </div>
+            <div className="mini-information">
+              <h1>314</h1>
+              <p>TEMPLATES</p>
+            </div>
+            <div className="mini-information">
+              <h1>12M+</h1>
+              <p>QUERIES</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
